c0ce8b582f2d992e2ca6c5a05bf8f8eaedefd001
Leon Bogaert <leon@omniboost.io>
2022-05-02 11:51:44 +0200

    Split types

--- doc.go
+++ doc.go
@@ -3,36 +3,47 @@ package hesta
 import "encoding/xml"
 
 type Respondents struct {
-	XMLName    xml.Name `xml:"Respondents"`
-	Respondent struct {
-		EstBurNr              string `xml:"EstBurNr"`
-		Period                string `xml:"Period"`
-		AutomaticTransmission string `xml:"AutomaticTransmission"`
-		RespondentData        struct {
-			Globals struct {
-				EstNbrRoom         string `xml:"EstNbrRoom"`
-				EstNbrBed          string `xml:"EstNbrBed"`
-				EstRoomForDisabled string `xml:"EstRoomForDisabled"`
-				EstContactName     string `xml:"EstContactName"`
-				EstContactPhone    string `xml:"EstContactPhone"`
-				EstContactEMail    string `xml:"EstContactEMail"`
-				EstNextClosingFrom string `xml:"EstNextClosingFrom"`
-				EstNextClosingTo   string `xml:"EstNextClosingTo"`
-				TotalArrivals      string `xml:"TotalArrivals"`
-				TotalNights        string `xml:"TotalNights"`
-				TotalOccupiedRooms string `xml:"TotalOccupiedRooms"`
-				EstAvgNightIncome  string `xml:"EstAvgNightIncome"`
-				TotalOpenDays      string `xml:"TotalOpenDays"`
-			} `xml:"Globals"`
-			Cursor struct {
-				ID  string `xml:"id,attr"`
-				Row []struct {
-					Text     string `xml:",chardata"`
-					Country  string `xml:"country"`
-					Arrivals string `xml:"arrivals"`
-					Nights   string `xml:"nights"`
-				} `xml:"Row"`
-			} `xml:"Cursor"`
-		} `xml:"RespondentData"`
-	} `xml:"Respondent"`
+	XMLName    xml.Name   `xml:"Respondents"`
+	Respondent Respondent `xml:"Respondent"`
+}
+
+type Respondent struct {
+	EstBurNr              string         `xml:"EstBurNr"`
+	Period                string         `xml:"Period"`
+	AutomaticTransmission string         `xml:"AutomaticTransmission"`
+	RespondentData        RespondentData `xml:"RespondentData"`
+}
+
+type RespondentData struct {
+	Globals Globals `xml:"Globals"`
+	Cursor  Cursor  `xml:"Cursor"`
+}
+
+type Globals struct {
+	EstNbrRoom         int     `xml:"EstNbrRoom"`
+	EstNbrBed          int     `xml:"EstNbrBed"`
+	EstRoomForDisabled int     `xml:"EstRoomForDisabled"`
+	EstContactName     string  `xml:"EstContactName"`
+	EstContactPhone    string  `xml:"EstContactPhone"`
+	EstContactEMail    string  `xml:"EstContactEMail"`
+	EstNextClosingFrom string  `xml:"EstNextClosingFrom"`
+	EstNextClosingTo   string  `xml:"EstNextClosingTo"`
+	TotalArrivals      int     `xml:"TotalArrivals"`
+	TotalNights        int     `xml:"TotalNights"`
+	TotalOccupiedRooms int     `xml:"TotalOccupiedRooms"`
+	EstAvgNightIncome  float64 `xml:"EstAvgNightIncome"`
+	TotalOpenDays      int     `xml:"TotalOpenDays"`
+}
+
+type Cursor struct {
+	ID  string `xml:"id,attr"`
+	Row Rows   `xml:"Row"`
+}
+
+type Rows []Row
+
+type Row struct {
+	Country  string `xml:"country"`
+	Arrivals int    `xml:"arrivals"`
+	Nights   int    `xml:"nights"`
 }
